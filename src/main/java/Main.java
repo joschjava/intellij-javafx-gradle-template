1db88a48f69ff000943aa378be338906139c385e
Jonas <no@spam.com>
2019-07-05 11:44:15 +0200

    Initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Test");
+        Scene scene = new Scene(label);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
